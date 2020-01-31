cfd98cd681e737fea3c495db9d64ac5a0da7bb49
Sebastian Kaiser <sk4is3r@gmail.com>
2020-01-31 22:52:47 +0100

    update asciidoctor setup

--- demos/cross-project-publications/buildSrc/src/main/kotlin/com/acme/support.kt
+++
@@ -1,6 +0,0 @@
-package com.acme
-
-import org.gradle.api.Named
-import org.gradle.api.Project
-
-inline fun <reified T : Named> Project.namedAttribute(value: String) = objects.named(T::class.java, value)
--- demos/publish-more/src/testFixtures/java/org/test/LibraryFixtures.java
+++
@@ -1,7 +0,0 @@
-package org.test;
-
-public class LibraryFixtures {
-    public static Library getNewLibrary() {
-        return new Library();
-    }
-}
